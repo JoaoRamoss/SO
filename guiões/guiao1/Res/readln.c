d90fb43eb659c5535916bea6d7ab7db406278ec3
João Pedro Ramos Monteiro SIlva <61993024+JoaoRamosUM@users.noreply.github.com>
2021-02-26 10:54:49 +0000

    Exercicio 3

--- guiões/guiao1/Res/readln.c
+++ guiões/guiao1/Res/readln.c
@@ -9,27 +9,25 @@
 ssize_t readln (int fd, char *line, size_t size) {
     int i = 0;
     char t = 0;
-    ssize_t tam = 0, res = 0;
+    ssize_t tam = 0;
    do {
        if (i == size-1) {
            size *= 2;
            line = realloc(line, sizeof(char)*size);
        }
         tam = read(fd, &t, 1);
-        if (t != '\n') {
+        if (t != '\n')
             line[i++] = t;
-            res++;
-        }
         else 
             break;
     } while (tam != 0);
-    return res;
+    return i;
 }
 
 int main (int argc, char *argv[]) {
     char *line = malloc(sizeof(char) * 1024);
     int fd_read = open(argv[1], O_RDONLY);
-    ssize_t tamanho_read = readln_alt(fd_read, line, sizeof(char)*1024);
+    ssize_t tamanho_read = readln(fd_read, line, sizeof(char)*1024);
     printf("%s\n", line);
     close(fd_read);
 }
